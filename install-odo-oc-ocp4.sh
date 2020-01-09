18007d7f5b82662aed0adc2d566514b9dd5eb848
Götz Rieger <grieger@redhat.com>
2020-01-09 11:41:19 +0100

    Update install-odo-oc-ocp4.sh

--- install-odo-oc-ocp4.sh
+++ install-odo-oc-ocp4.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Run with: curl -Ls https://raw.githubusercontent.com/goetzrieger/snippets/master/install-odo-oc-ocp4.sh -o /tmp/install-odo-oc-ocp4.sh && sh /tmp/install-odo-oc-ocp4.sh
+
+# Make sure ~/bin exists and is in $PATH
+echo 'Checking ~/bin is in $PATH'
 mkdir -p ~/bin
 case ":$PATH:" in *:$HOME/bin:*) echo "~/bin already in \$PATH";; *) PATH=$PATH:~/bin ;; esac
 
-
-curl -L https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.2/linux/oc.tar.gz -o /tmp/oc.tar.gz && \
+# get and install odo and oc
+echo "Downloading and installing oc"
+curl -Ls https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.2/linux/oc.tar.gz -o /tmp/oc.tar.gz && \
 tar -xf /tmp/oc.tar.gz -C ~/bin/ && \
 chmod +x ~/bin/oc
 
-curl -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64.tar.gz -o /tmp/odo.tar.gz && \
+echo "Downloading and installing odo"
+curl -Ls https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64.tar.gz -o /tmp/odo.tar.gz && \
 tar -xf /tmp/odo.tar.gz -C ~/bin/ && \
 chmod +x ~/bin/odo
 
